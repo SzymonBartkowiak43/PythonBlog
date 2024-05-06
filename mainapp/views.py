491964fe476ab0f5057cac274c5c65b54fd9dec5
G0dles43 <85917@student.pb.edu.pl>
2024-05-06 16:23:53 +0200

    16:23

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render, redirect
 from django.http import *
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from .forms import CustomUserCreationForm
 
 def homepage(request):
     return render(request, "home.html")
 
+
+
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "register.html", { "form": form})
+
+
 
 def login(request):
     return  render(request, "login.html")
