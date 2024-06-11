ee4a726ed3c87ded967f3a32231504ab3a301887
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-06-11 12:35:34 +0200

    TESTY

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post
+from .models import Blog
 
-# Create your tests here.
+class BlogCreationViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_blog_creation_view_with_valid_data(self):
+        response = self.client.post(reverse('utworz_blog'), data={'title': 'Testowy tytuł', 'content': 'Testowa treść'})
+        self.assertEqual(response.status_code, 200)  # Oczekiwany kod statusu przekierowania
+
+    def test_example(self):
+        self.assertTrue(True)  # Przykładowy test, który zawsze przechodzi
+
+    def test_user(self):
+        self.assertEqual(self.user.username, 'testuser')
+    def test_szyfrowanie_hasla(self):
+        self.assertNotEquals(self.user.password, 'testpassword')
